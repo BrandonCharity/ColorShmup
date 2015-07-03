5361b47acf5bfbd321fe4406a844ded59b76401f
bcharity <bcharity@ijgn.jp>
2015-07-03 12:24:40 +0900

    Added player movement script

--- ColorShmup/Assets/Scripts/Ship.cs
+++ ColorShmup/Assets/Scripts/Ship.cs
@@ -1,15 +1,28 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class Ship : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+	/// <summary>
+	/// 1 - The speed of the ship
+	/// </summary>
+	public Vector2 speed;
 	
+	// 2 - Store the movement
+	private Vector2 movement;
+	
+	void Update()
+	{
+		// 3 - Retrieve axis information
+		float inputX = Input.GetAxis("Horizontal");
+		float inputY = Input.GetAxis("Vertical");
+		
+		// 4 - Movement per direction
+		movement = new Vector2(
+			speed.x * inputX,
+			speed.y * inputY);
+
+		transform.position += new Vector3 (movement.x, movement.y, transform.position.z);
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
 }

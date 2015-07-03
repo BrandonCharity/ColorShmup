ad3f8eae46d5fa374faa8f12fd9788fcba8ba4e9
bcharity <brandon.charity@gmail.com>
2015-07-03 15:13:30 +0900

    Changed bullet movement script.

---
+++ ColorShmup/Assets/Scripts/BulletMovement.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletMovement : MonoBehaviour {
+
+	public float speed;
+	public Direction direction;
+
+	[HideInInspector]
+	public enum Direction {
+		Up, Down, Left, Right
+	};
+
+	private Vector3 vectorToAdd;
+
+	void Awake() {
+		if (direction == Direction.Down) {
+			vectorToAdd = new Vector3 (0, -1 * speed * Time.deltaTime, 0);
+		} else if (direction == Direction.Left) {
+			vectorToAdd = new Vector3 (-1 * speed * Time.deltaTime, 0, 0);
+		} else if (direction == Direction.Right) {
+			vectorToAdd = new Vector3 (speed * Time.deltaTime, 0, 0);
+		} else {
+			vectorToAdd = new Vector3 (0, speed * Time.deltaTime, 0);
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		transform.position += vectorToAdd;
+	}
+
+	void OnBecameInvisible() {
+		Destroy (this.gameObject);
+	}
+}
